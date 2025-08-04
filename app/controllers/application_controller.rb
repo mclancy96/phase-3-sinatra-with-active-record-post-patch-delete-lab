f6e976f8abf837a4cbc99b1522f2b96260d771d7
Mike <mclancy96@gmail.com>
2025-08-04 10:27:23 -0400

    Delete route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,4 +34,12 @@ class ApplicationController < Sinatra::Base
     bakery.update(name: params[:name])
     bakery.to_json
   end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find_by(id: params[:id])
+    return unless baked_good
+
+    baked_good.destroy
+    baked_good.to_json
+  end
 end
